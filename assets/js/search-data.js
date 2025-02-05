a175a84984f39ca909b4aa674050e60921d3f1be
ysnrski <94907957+ysnrski@users.noreply.github.com>
2025-02-05 09:50:16 +0000

    Deploying to gh-pages from @ ysnrski/ysnrski.github.io@d6de65354df6a2a44f44dfd3f3c4a1c138894d30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-service",
           title: "service",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/service/";
