02056262d28e78e52386cb6cce1553cba1c60a03
ysnrski <94907957+ysnrski@users.noreply.github.com>
2025-02-05 08:52:26 +0000

    Deploying to gh-pages from @ ysnrski/ysnrski.github.io@6fa7e012a6d6245b249a920c727f443d52561890 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "research",
-          description: "Test",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
