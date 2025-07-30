3d82c39e835e623ec3b53b2217267bd481218c8a
ysnrski <94907957+ysnrski@users.noreply.github.com>
2025-07-30 08:05:13 +0000

    Deploying to gh-pages from @ ysnrski/ysnrski.github.io@32008dc9432c0ecb788fa8703d309829c1d2648a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-publications",
           title: "publications",
           description: "",
